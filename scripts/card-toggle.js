758a3a303f25d42dd035845dc07fb6bad29c603d
KMS <y.dudar@list.ru>
2025-03-02 16:17:54 +0300

    new fix

--- scripts/card-toggle.js
+++ scripts/card-toggle.js
@@ -1,26 +1,47 @@
 export default class CardToggle {
   selectors = {
-      card: '.list__item',
-      description: '.focus-info__description',
-  }
+    card: ".list__item",
+    description: ".focus-info__description",
+  };
 
   stateClasses = {
-      isActive: 'is-active',
-  }
+    isActive: "is-active",
+  };
 
   constructor() {
-      this.cards = document.querySelectorAll(this.selectors.card);
-      this.bindEvents();
+    this.cards = document.querySelectorAll(this.selectors.card);
+    this.bindEvents();
+    this.bindResizeEvent();
   }
 
   toggleCard = (event) => {
-      const card = event.currentTarget;
-      card.classList.toggle(this.stateClasses.isActive);
-  }
+    const card = event.currentTarget;
+    card.classList.toggle(this.stateClasses.isActive);
+  };
+
+  isActive = (event) => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 1440) {
+      this.toggleCard(event);
+    }
+  };
+
+  resetActiveCards = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 1440) {
+      this.cards.forEach((card) => {
+        card.classList.remove(this.stateClasses.isActive);
+      });
+    }
+  };
 
   bindEvents() {
-      this.cards.forEach((card) => {
-          card.addEventListener('click', this.toggleCard);
-      });
+    this.cards.forEach((card) => {
+      card.addEventListener("click", this.isActive);
+    });
+  }
+
+  bindResizeEvent() {
+    window.addEventListener("resize", this.resetActiveCards);
   }
 }
